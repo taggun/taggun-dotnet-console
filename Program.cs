c7269a9c72a19051d71ab2dacca98f2437cca1dd
Chao Koon Lee <chaokoon@gmail.com>
2017-08-02 01:40:26 +1200

    First commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace code_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = @"C:\temp\test.jpg";
+            var taggunApiKey = "<--ENTER YOUR API KEY HERE -->";
+            var taggunApiUrl = "https://api.taggun.io/api/receipt/v1/simple/file";
+
+            byte[] fileData = System.IO.File.ReadAllBytes(fileName);
+
+            var timeStart = DateTime.Now;
+
+            using (var httpClient = new HttpClient { Timeout = new TimeSpan(0, 0, 0, 60, 0) })
+            {
+                HttpResponseMessage response = null;
+
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("apikey", taggunApiKey);
+
+                var parentContent = new MultipartFormDataContent("----WebKitFormBoundaryfzdR3Imh7urK8qw");
+
+                var documentContent = new ByteArrayContent(fileData);
+                documentContent.Headers.Remove("Content-Type");
+                documentContent.Headers.Remove("Content-Disposition");
+                documentContent.Headers.TryAddWithoutValidation("Content-Type", "image/jpeg");
+                documentContent.Headers.TryAddWithoutValidation("Content-Disposition",
+                string.Format(@"form-data; name=""file""; filename=""{0}""", "testfilename.jpg"));
+                parentContent.Add(documentContent);
+
+                var refreshContent = new StringContent("false");
+                refreshContent.Headers.Remove("Content-Type");
+                refreshContent.Headers.Remove("Content-Disposition");
+                refreshContent.Headers.TryAddWithoutValidation("Content-Disposition", @"form-data; name=""refresh""");
+                parentContent.Add(refreshContent);
+
+                response = httpClient.PostAsync(taggunApiUrl, parentContent).Result;
+                response.EnsureSuccessStatusCode();
+                var result = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JObject.Parse(result).ToString());
+                    
+              
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# taggun-dotnet-console is built for [Taggun](https://www.taggun.io) Receipt OCR
+
+This is a sample application to make a `multipart/form-data` request to Taggun.
+
+Built on .NET core using VSCode.
+
